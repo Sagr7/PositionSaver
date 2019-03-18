f11976cfd39b00578ec2ebc418e813c94cbfa6ed
Sagr7 <48094280+Sagr7@users.noreply.github.com>
2019-03-18 09:23:05 +0200

    Update draw.lua

--- positionsaver/draw.lua
+++ positionsaver/draw.lua
@@ -1,4 +1,52 @@
---This file is TODO
---addEventHandler("onClientRender", function()
-  --will be the time written on the screen
---end)
+
+local notesDuration = 2000
+local screenX, screenY = guiGetScreenSize()
+
+local someTimer = nil
+local someText = ""
+
+function addNote(text)
+	
+	if isEventHandlerAdded("onClientRender", root, drawNotes) then
+		removeEventHandler("onClientRender", root, drawNotes)
+		someText = ""
+		if isTimer(someTimer) then
+			killTimer(someTimer)
+		end
+	end
+	
+	someText = text
+	addEventHandler("onClientRender", root, drawNotes)
+	
+	someTimer = setTimer(function()
+		if isEventHandlerAdded("onClientRender", root, drawNotes) then
+			removeEventHandler("onClientRender", root, drawNotes)
+			someText = ""
+		end
+	end, notesDuration, 1)
+
+end
+
+function drawNotes()
+	if someText and tostring(someText) and string.len(someText) > 3 then
+		dxDrawRectangle(40, (screenY/2) - 100, dxGetTextWidth(someText, 0.8, "bankgothic", true) + 15, 25, tocolor(0,0,0, 180))
+		dxDrawText(someText, 50, (screenY/2) - 100, _, _, _, 0.8, "bankgothic", _, _, _, _, _,true)
+	end
+	
+end
+
+
+
+function isEventHandlerAdded( sEventName, pElementAttachedTo, func )
+     if type( sEventName ) == 'string' and isElement( pElementAttachedTo ) and type( func ) == 'function' then
+          local aAttachedFunctions = getEventHandlers( sEventName, pElementAttachedTo )
+          if type( aAttachedFunctions ) == 'table' and #aAttachedFunctions > 0 then
+               for i, v in ipairs( aAttachedFunctions ) do
+                    if v == func then
+        	         return true
+        	    end
+	       end
+	  end
+     end
+     return false
+end
